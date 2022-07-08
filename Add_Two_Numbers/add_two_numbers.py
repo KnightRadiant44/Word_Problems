224145b77e04935e708a603de3425d6110ea097c
KnightRadiant44 <mahir.islam.numayeer@gmail.com>
2022-07-08 21:42:10 +0600

    "Add Two Numbers" folder and solution file added

---
+++ Add_Two_Numbers/add_two_numbers.py
@@ -0,0 +1,10 @@
+T = int(input())
+for tc in range(T):
+	# Read integers a and b.
+	(a, b) = map(int, input().split(' '))
+	
+	ans = a + b
+	print(ans)
+range(T)
+
+#Link to the problem: https://www.codechef.com/problems/FLOW001
