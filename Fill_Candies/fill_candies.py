dea8ac6574a7aec9b7bed1be774b247946b0b6c4
KnightRadiant44 <mahir.islam.numayeer@gmail.com>
2022-07-26 07:08:12 +0800

    fill_candies

---
+++ Fill_Candies/fill_candies.py
@@ -0,0 +1,9 @@
+import math
+T = int(input())
+for tc in range(T):
+    (n, k, m) = map(int, input().split(' '))
+    
+    dawn = k * m
+    hate_morning = n / dawn
+    rounding = math.ceil(hate_morning)
+    print(rounding)
